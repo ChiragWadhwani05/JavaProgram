4d071bb73e78137b1c121c76b5f91fa9d5921f32
chirag0508 <wadhwanichirag94@gmail.com>
2022-08-12 16:32:37 +0000

    New Update to hello file

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class hello{
     public static void main(String[] args) {
-        System.out.println("Hello kali");
+        System.out.println("Hello kali Linux");
+        System.out.println("New Line");
+        System.out.println("SEcond line");
     }
 }
